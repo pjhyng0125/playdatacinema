7ac84fc9a211c933a71b0781555d39ff0188bbff
hoon7018 <40628980+hoon7018@users.noreply.github.com>
2018-06-29 17:37:35 +0900

    Add files via upload

---
+++ test.java
@@ -0,0 +1,10 @@
+package com.encore;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("hi lm here");
+		System.out.println("check");
+		System.out.println("check222");
+		System.out.println("0629/ check");
+	}
+}
