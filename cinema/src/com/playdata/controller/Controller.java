c05d9143ab043c9e7c58fc8d0931e486515a788c
pjhyng0125 <pjhyng0125@naver.com>
2018-06-30 23:43:53 +0900

    modify java
    modify java

--- cinema/src/com/playdata/controller/Controller.java
+++ cinema/src/com/playdata/controller/Controller.java
@@ -1,9 +1,13 @@
 package com.playdata.controller;
 
+import com.playdata.view.LoginView;
+
 public class Controller {
+	
 	public Controller() {
-		
+		new LoginView();
 	}
+	
 	public static void main(String[] args) {
 		new Controller();
 	}
---
+++ cinema/src/com/playdata/model/MovieDAO.java
@@ -0,0 +1,11 @@
+package com.playdata.model;
+/*
+ * 무비DAO: 영화의 정보 - DB
+ */
+public class MovieDAO {
+	
+	public MovieDAO() {
+		System.out.println("git 연습 발견시 삭제하고 프로그래밍 하시면 됩니다.");
+		
+	}
+}
