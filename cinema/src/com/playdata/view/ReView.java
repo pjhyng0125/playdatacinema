1c6d295648dbae56f640e223d1cad2ebf699e7be
pjhyng0125 <pjhyng0125@naver.com>
2018-07-02 20:28:05 +0900

    update ReView
    update ReView
    fail to add Scrollbar

--- cinema/src/com/playdata/view/ReView.java
+++ cinema/src/com/playdata/view/ReView.java
@@ -1,48 +1,73 @@
 package com.playdata.view;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.util.Vector;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 /*
  * 후기뷰
  */
 public class ReView extends JFrame {
-	JPanel p_content;
-	JScrollPane sp_frame;
+	JPanel p_content, p_reviews;
+	JScrollPane sp;
 	Vector<ReviewSubView> v;
+	
 	public ReView() {
 		setTitle("ReView");
+//new
 		p_content = new JPanel();
-		p_content.setBackground(Color.CYAN);
+		p_reviews = new JPanel();
 		v = new Vector<>();
-		sp_frame = new JScrollPane();
-//		sp_frame.setViewportView(this);
+
+//setBackground
+		p_content.setBackground(Color.CYAN);
+		p_reviews.setBackground(Color.green);
+		p_reviews.setPreferredSize(new Dimension(1140, 250));
+		
+//setlayout
+		p_reviews.setLayout(null);
 		setLayout(null);
-		  
+		
+//setSize
 		p_content.setBounds(20,20,1140,350);
 		
+//JScrollerPane
+		sp = new JScrollPane();
+		sp.setViewportView(p_reviews);
+
+		sp.setPreferredSize(new Dimension(1140, 250));
+		sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+//		sp.setVerticalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		sp.setVisible(true);
+		sp.setBounds(20, 370, 1140, 350);
+		add(sp);
+		
+//addReviewSubView
 		ReviewSubView rsv = new ReviewSubView();
 		ReviewSubView rsv1 = new ReviewSubView();
 		ReviewSubView rsv2 = new ReviewSubView();
 		ReviewSubView rsv3 = new ReviewSubView();
 		ReviewSubView rsv4 = new ReviewSubView();
+//		ReviewSubView rsv5 = new ReviewSubView();
 		v.add(rsv);
 		v.add(rsv1);
 		v.add(rsv2);
 		v.add(rsv3);
 		v.add(rsv4);
-//sp_frame
+//		v.add(rsv5);
+
+//add to frame
 		add(p_content);
 		for(int i=0; i<v.size(); i++) {
-			v.get(i).setBounds(20, 100*(i+1)+280, 1140, 100);
-			add(v.get(i));
+//			v.get(i).setBounds(20, 100*(i+1)+280, 1140, 100);
+			v.get(i).setBounds(20, 100*(i), 1140, 100);			
+			p_reviews.add(v.get(i));
 		}
-//		sp_frame.setBackground(Color.pink);
-//		add("Center",sp_frame);
+		
 		setSize(1200, 800);
 		setVisible(true);
 	}//생성자
