697033705e5d82428427741d916fa05e386645f5
wooyoung92 <40686749+wooyoung92@users.noreply.github.com>
2018-07-03 17:43:47 +0900

    우영
    스크린 1.0
    결제창 1.0

--- cinema/src/com/playdata/view/PayView.java
+++ cinema/src/com/playdata/view/PayView.java
@@ -1,19 +1,285 @@
 package com.playdata.view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import javax.swing.JList;
+import java.awt.Choice;
 /*
  * 결제뷰
  */
 public class PayView extends JFrame {
+	JPanel p;
+	JPanel p_cash,p_discount;
+	
+	//-----------------------------라벨
+	JLabel la_member;//회원등급
+	JLabel la_pay;//결제금액
+	JLabel la_discount;//할인금액
+	JLabel la_point;//포인트사용
+	JLabel la_final_pay;//최종결제금액
+	JLabel la_have_cash;//보유 캐쉬
+	JLabel la_balance;// 사용후 잔액
+	JLabel la_sc_point;// 지급예정포인트
+	JLabel la_cash;//왼쪽 캐쉬
+	JLabel la_charge_cash;//캐쉬충전
+	JLabel la_discount_list, la_discount_coupon,la_have_point;//할인내역,할인쿠폰,보유포인트
+	Choice ch_copoun;//쿠폰 선택
+	
+	//-----------------------------텍스트에어리어
+	JTextArea ta_grade;//등급정보
+	JTextArea ta_pmovie_info;//영화정보
+    JTextArea ta_pay;//결제금액
+    JTextArea ta_discount;//할인금액
+    JTextArea ta_point;//포인트금액
+    JTextArea ta_final_pay;//최종결제금액
+    JTextArea ta_have_cash;//보유캐쉬
+    JTextArea ta_balance;//사용후 잔액
+    JTextArea ta_sc_point;//지급예정포인트
+    
+    //------------------------------텍스트필드
+    JTextField tf_charge_cash;//캐쉬충전
+    JTextField tf_use_point;//포인트 사용
+    
+    //-------------------------------버튼
+    JButton b_ok,b_cancel;//오른쪽 확인,취소
+    JButton b_charge_ok,b_charge_cancel;//캐쉬 충전 취소
+    JButton b_discount_ok,b_discount_cancel;//할인 쿠폰 사용 취소
+    JButton b_point_ok,b_point_cancel;//포인트 사용 취소
+    private JPanel panel_1;
+
+	
+	
+	
 	public PayView() {
-		setTitle("PayView");
-		System.out.println("지환연습");
-		 ////////
-		System.out.println("동일페이지 수정연습");
+		setTitle("결제창");
+
+//============ 결제 진행 정보 ===========
+		p = new JPanel();
+			p.setBackground(Color.WHITE);
+			p.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLACK, 
+					Color.BLACK, Color.BLACK, Color.BLACK));
+			p.setBounds(667, 40, 470, 652);
+		la_member = new JLabel("회원등급");
+			la_member.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		ta_pmovie_info = new JTextArea();
+			ta_pmovie_info.setText("영화정보 (날짜,시작시간,상영관)");
+		ta_grade = new JTextArea();
+			ta_grade.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+			ta_grade.setText("등급정보");
+		la_pay = new JLabel("결제금액");
+			la_pay.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		la_discount = new JLabel("할인금액");
+			la_discount.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		la_point = new JLabel("포인트사용");
+			la_point.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		la_final_pay = new JLabel("최종결제금액");
+			la_final_pay.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		la_have_cash = new JLabel("보유 캐쉬");
+			la_have_cash.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		la_balance = new JLabel("사용후 잔액");
+			la_balance.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		la_sc_point = new JLabel("지급예정포인트");
+			la_sc_point.setFont(new Font("맑은 고딕", Font.PLAIN, 19));
 		
-		System.out.println("마지막");
+		//텍스트에어이어	
+		ta_pay = new JTextArea();
+		    ta_pay.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		    ta_pay.setText("16,000");	
+		ta_discount = new JTextArea();
+			ta_discount.setText("0");
+			ta_discount.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		ta_point = new JTextArea();
+			ta_point.setText("2,000");
+			ta_point.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		ta_final_pay = new JTextArea();
+			ta_final_pay.setText("14,000");
+			ta_final_pay.setFont(new Font("맑은 고딕", Font.PLAIN, 22));			
+		ta_have_cash = new JTextArea();
+			ta_have_cash.setText("30,000");
+			ta_have_cash.setFont(new Font("맑은 고딕", Font.PLAIN, 22));			
+		ta_balance = new JTextArea();
+			ta_balance.setText("16,000");
+			ta_balance.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		ta_sc_point = new JTextArea();
+			ta_sc_point.setText("140");
+			ta_sc_point.setFont(new Font("맑은 고딕", Font.PLAIN, 21));			
+		
+			
+	    //버튼
+		b_ok = new JButton("확인");   		   //최종결제 확인
+		b_cancel = new JButton("취소");		   //최종결제 취소
+		b_charge_ok = new JButton("충전");	   //충전 확인
+		b_charge_cancel = new JButton("취소");  //충전 취소
+		b_discount_ok = new JButton("사용");    //쿠폰 사용
+		b_discount_cancel = new JButton("취소");//쿠폰 취소
+		b_point_ok = new JButton("사용");       //포인트 사용
+		b_point_cancel = new JButton("취소");   //포인트 취소
+		
+		//라벨위치
+		la_member.setBounds(148, 30, 121, 54);//회원등급
+		la_pay.setBounds(40, 170, 121, 41);//결제금액
+		la_discount.setBounds(40, 242, 121, 41);//할인금액
+		la_point.setBounds(40, 306, 136, 41);//포인트금액
+		la_final_pay.setBounds(40, 368, 161, 41);//최종결제금액
+		la_have_cash.setBounds(40, 437, 161, 41);//보유캐쉬
+		la_balance.setBounds(40, 476, 161, 41);//사용후잔액
+		la_sc_point.setBounds(40, 529, 161, 41);//지급예정포인트
+		
+		//텍스트에어리어	
+		ta_grade.setBounds(253, 40, 161, 34);//등급정보
+		ta_pmovie_info.setBounds(86, 96, 311, 35);//영화정보
+		ta_pay.setBounds(252, 170, 110, 37);//결제금액
+		ta_discount.setBounds(253, 242, 110, 37);//할인금액
+		ta_point.setBounds(252, 310, 110, 37);//포인트금액
+		ta_final_pay.setBounds(253, 372, 110, 37);//최종결제금액
+		ta_have_cash.setBounds(252, 441, 110, 37);//보유캐쉬
+		ta_balance.setBounds(252, 476, 110, 37);//사용후잔액
+		ta_sc_point.setBounds(252, 529, 110, 37);//지급예정포인트
+			
+		//버튼 위치
+		b_ok.setBounds(118, 599, 105, 41);    		 //최종결제 확인
+		b_cancel.setBounds(271, 599, 105, 41); 		 //최종결제 취소
+		b_charge_ok.setBounds(432, 264, 61, 27);     //충전 확인
+		b_charge_cancel.setBounds(507, 264, 61, 27); //충전 취소
+		b_discount_ok.setBounds(432, 500, 61, 27);   //쿠폰 사용
+		b_discount_cancel.setBounds(507, 500, 61, 27);//쿠폰 취소
+		b_point_ok.setBounds(432, 586, 61, 27);      //포인트 사용
+		b_point_cancel.setBounds(507, 586, 61, 27);  //포인트 취소
+
+
+
+		
+		//contentpane
+	    getContentPane().add(p);
+	    getContentPane().setLayout(null);
+	    getContentPane().add(b_charge_ok);
+		getContentPane().add(b_charge_cancel);
+		getContentPane().add(b_point_ok);
+		getContentPane().add(b_point_cancel);
+		getContentPane().add(b_discount_ok);
+		getContentPane().add(b_discount_cancel);
+		
+	    
+		p.setLayout(null);
+		p.add(la_member);
+		p.add(ta_pmovie_info);
+		p.add(ta_grade);
+		p.add(la_pay);
+		p.add(la_discount);
+		p.add(la_point);
+		p.add(la_final_pay);
+		p.add(la_have_cash);
+		p.add(la_balance);
+		p.add(la_sc_point);
+		p.add(b_ok);
+		p.add(b_cancel);
+		p.add(ta_pay);
+		p.add(ta_discount);
+		p.add(ta_point);
+		p.add(ta_final_pay);
+		p.add(ta_have_cash);
+		p.add(ta_balance);
+		p.add(ta_sc_point);
+		
+		
+        //왼쪽 캐쉬 충전, 할인내역 라벨
+		la_cash = new JLabel("캐쉬");
+			la_cash.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		la_have_cash = new JLabel("보유캐쉬");
+			la_have_cash.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		la_charge_cash = new JLabel("캐쉬 충전");
+		la_charge_cash.setForeground(SystemColor.desktop);
+			la_charge_cash.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		la_discount_list = new JLabel("할인내역");
+			la_discount_list.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		la_discount_coupon = new JLabel("할인 쿠폰");
+			la_discount_coupon.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		la_have_point = new JLabel("보유 포인트");
+			la_have_point.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		ta_have_cash = new JTextArea();
+			ta_have_cash.setBackground(SystemColor.text);
+			ta_have_cash.setText("30,000");	
+		ta_have_cash = new JTextArea();
+			ta_have_cash.setBackground(SystemColor.text);
+			ta_have_cash.setText("30,000");
+			ta_have_cash.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+		ta_have_cash = new JTextArea();
+			ta_have_cash.setBackground(SystemColor.text);
+			ta_have_cash.setText("30,000");
+			ta_have_cash.setFont(new Font("맑은 고딕", Font.PLAIN, 22));
+			
+		//왼쪽 라벨 위치
+		la_cash.setBounds(101, 84, 109, 66);
+		la_have_cash.setBounds(101, 162, 109, 66);
+		la_charge_cash.setBounds(101, 240, 109, 66);
+		la_discount_list.setBounds(101, 406, 109, 66);
+		la_discount_coupon.setBounds(101, 484, 109, 66);
+		la_have_point.setBounds(101, 572, 138, 66);	
+		ta_have_cash.setBounds(272, 178, 110, 37);
+		
+		//contentpane
+		getContentPane().add(la_cash);
+		getContentPane().add(la_have_cash);
+		getContentPane().add(la_charge_cash);
+		getContentPane().add(la_discount_list);
+		getContentPane().add(la_discount_coupon);
+		getContentPane().add(la_have_point);
+		getContentPane().add(ta_have_cash);
+		
+		
+		
+		//텍스트필드
+		tf_charge_cash = new JTextField();
+			tf_charge_cash.setBounds(253, 258, 129, 31);
+			getContentPane().add(tf_charge_cash);
+			tf_charge_cash.setColumns(10);
+		tf_use_point = new JTextField();
+			tf_use_point.setBounds(253, 584, 129, 31);
+			getContentPane().add(tf_use_point);
+			tf_use_point.setColumns(10);
+		
+		//초이스
+		ch_copoun= new Choice();
+			ch_copoun.add("===== 선택 =====");
+			ch_copoun.add("생일 50%할인 쿠폰");
+			ch_copoun.add("생일 50%할인 쿠폰");
+			ch_copoun.add("생일 50%할인 쿠폰");
+			ch_copoun.add("생일 50%할인 쿠폰");
+			ch_copoun.setBounds(262, 503, 120, 24);
+			getContentPane().add(ch_copoun);
+			
+		//패널 테두리	
+			p_cash = new JPanel();
+				p_cash.setBackground(Color.white);
+				p_cash.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLACK,Color.BLACK, Color.BLACK, Color.BLACK));
+				p_cash.setBounds(77, 84, 532, 259);
+				getContentPane().add(p_cash);
+				p_cash.setLayout(null);
+			p_discount = new JPanel();
+				p_discount.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK));
+				p_discount.setBackground(Color.WHITE);
+				p_discount.setBounds(77, 406, 532, 259);
+				getContentPane().add(p_discount);
+				p_discount.setLayout(null);
+			
 		
 		setSize(1200, 800);
 		setVisible(true);
+		
 	}//생성자
+	
+	public static void main(String[] args) {
+		new PayView();
+	}
 }
--- cinema/src/com/playdata/view/ScreenView.java
+++ cinema/src/com/playdata/view/ScreenView.java
@@ -26,21 +26,21 @@ public class ScreenView extends JFrame {
 	JButton select_movie,pay_view;//페이지 이동
 
 	//---------------------------------------라벨
-	JLabel a_l,b_l,c_l;//좌석라벨
-	JLabel select_l,Unselectable_l,selecting_l;//선택,가능 불가능 라벨
+	JLabel la_a,la_b,la_c;//좌석라벨
+	JLabel la_select,la_unselectable,la_selecting;//선택,가능 불가능 라벨
 	JLabel image_select, image_unselecting,image_selecting;//선택,가능 불가능 이미지 라벨
-	JLabel screen_l;//스크린 라벨
-	JLabel theater_l;//1관,2관....
-	JLabel movie_image;//영화 포스터
+	JLabel la_screen;//스크린 라벨
+	JLabel la_theater;//1관,2관....
+	JLabel la_movie_image;//영화 포스터
 	
 	//--------------------------------------패널
 	JPanel p;//screen 패널
 	
 	//--------------------------------------텍스트에어리어
-	JTextArea info_ta;//상영관정보
-	JTextArea seat_info_ta;//좌석정보
-	JTextArea select_pay_ta;//선택가격
-	JTextArea movie_info_ta;//영화정보
+	JTextArea ta_info;//상영관정보
+	JTextArea ta_seat_info;//좌석정보
+	JTextArea ta_select_pay;//선택가격
+	JTextArea ta_movie_info;//영화정보
 
 	public ScreenView() {
 		setTitle("ScreenView");
@@ -144,12 +144,12 @@ public class ScreenView extends JFrame {
 			seat_c10.setBackground(Color.BLUE);
 		
 		//abc 라벨
-		a_l = new JLabel(" A");
-			a_l.setFont(new Font("굴림", Font.PLAIN, 25));
-		b_l = new JLabel(" B");
-			b_l.setFont(new Font("굴림", Font.PLAIN, 25));
-		c_l = new JLabel(" C");
-			c_l.setFont(new Font("굴림", Font.PLAIN, 25));
+		la_a = new JLabel(" A");
+			la_a.setFont(new Font("굴림", Font.PLAIN, 25));
+		la_b = new JLabel(" B");
+			la_b.setFont(new Font("굴림", Font.PLAIN, 25));
+		la_c = new JLabel(" C");
+			la_c.setFont(new Font("굴림", Font.PLAIN, 25));
 		
 		//페이지 이동
 		select_movie = new JButton("영화선택 (이전페이지)");
@@ -160,9 +160,9 @@ public class ScreenView extends JFrame {
 			pay_view.setForeground(Color.WHITE);
 		
 		//선택,불가능,가능 라벨
-		select_l = new JLabel("선택중");
-		Unselectable_l = new JLabel("예매(선택불가)");
-		selecting_l = new JLabel("선택가능");
+		la_select = new JLabel("선택중");
+		la_unselectable = new JLabel("예매(선택불가)");
+		la_selecting = new JLabel("선택가능");
 		
 		//선택,불가능,가능 이미지
 		image_select = new JLabel("image");
@@ -209,18 +209,18 @@ public class ScreenView extends JFrame {
 	
 		
 		//abc 라벨 위치
-		a_l.setBounds(216, 432, 43, 37);
-		b_l.setBounds(216, 498, 43, 37);
-		c_l.setBounds(216, 568, 43, 37);
+		la_a.setBounds(216, 432, 43, 37);
+		la_b.setBounds(216, 498, 43, 37);
+		la_c.setBounds(216, 568, 43, 37);
 		
 		//페이지 이동 위치
 		select_movie.setBounds(48, 816, 170, 125);
 		pay_view.setBounds(1063, 816, 170, 125);
 
 		//선택,불가능,가능 라벨 위치
-		select_l.setBounds(91, 441, 62, 18);
-		Unselectable_l.setBounds(88, 509, 114, 18);
-		selecting_l.setBounds(88, 577, 62, 18);
+		la_select.setBounds(91, 441, 62, 18);
+		la_unselectable.setBounds(88, 509, 114, 18);
+		la_selecting.setBounds(88, 577, 62, 18);
 		
 		//선택,불가능,가능 이미지 위치
 		image_select.setBounds(14, 441, 62, 18);
@@ -267,18 +267,18 @@ public class ScreenView extends JFrame {
 		getContentPane().add(seat_c10);
 		
 		//abc라벨 contentpane
-		getContentPane().add(a_l);
-		getContentPane().add(b_l);
-		getContentPane().add(c_l);
+		getContentPane().add(la_a);
+		getContentPane().add(la_b);
+		getContentPane().add(la_c);
 		
 		//페이지 이동 contentpane
 		getContentPane().add(select_movie);
 		getContentPane().add(pay_view);
 
 		//선택,불가능,가능 라벨 contentpane
-		getContentPane().add(select_l);
-		getContentPane().add(Unselectable_l);
-		getContentPane().add(selecting_l);
+		getContentPane().add(la_select);
+		getContentPane().add(la_unselectable);
+		getContentPane().add(la_selecting);
 		
 		//선택,불가능,가능 이미지 contentpane
 		getContentPane().add(image_select);
@@ -288,7 +288,7 @@ public class ScreenView extends JFrame {
 		
 		
 //============================ screen ================================
-		screen_l = new JLabel("screen");
+		la_screen = new JLabel("screen");
 		p = new JPanel();
 			p.setBackground(Color.WHITE);
 			getContentPane().add(p);
@@ -297,50 +297,50 @@ public class ScreenView extends JFrame {
 		
 		//위치
 		p.setBounds(273, 339, 850, 37);
-		screen_l.setBounds(400, 12, 44, 18);
-		p.add(screen_l);
+		la_screen.setBounds(400, 12, 44, 18);
+		p.add(la_screen);
 			
 		
 //=========================== 스크린 정보 ===================================		
-		info_ta = new JTextArea();
-		theater_l = new JLabel("<1관>");
-			theater_l.setFont(new Font("맑은 고딕", Font.PLAIN, 30));
+		ta_info = new JTextArea();
+		la_theater = new JLabel("<1관>");
+			la_theater.setFont(new Font("맑은 고딕", Font.PLAIN, 30));
 		
 			//텍스트 에어리어
-		info_ta.setText("영화 정보 (남은좌석,날짜,상영시간)");
+		ta_info.setText("영화 정보 (남은좌석,날짜,상영시간)");
 			
 		//위치
-		info_ta.setBounds(390, 92, 590, 208);
-		theater_l.setBounds(642, 43, 113, 37);
+		ta_info.setBounds(390, 92, 590, 208);
+		la_theater.setBounds(642, 43, 113, 37);
 		
 		//contentpane
-		getContentPane().add(info_ta);
-		getContentPane().add(theater_l);
+		getContentPane().add(ta_info);
+		getContentPane().add(la_theater);
 		
 		
 //========================== 가격, 좌석, 영화 정보 ============================
-		select_pay_ta = new JTextArea();
-		seat_info_ta = new JTextArea();
-		movie_image = new JLabel("image");
-		movie_info_ta = new JTextArea();
+		ta_select_pay = new JTextArea();
+		ta_seat_info = new JTextArea();
+		la_movie_image = new JLabel("image");
+		ta_movie_info = new JTextArea();
 		
 		//텍스트 에어리어
-		select_pay_ta.setText("선택가격");
-		seat_info_ta.setText("좌석정보");
-		movie_info_ta.setText("영화정보");
+		ta_select_pay.setText("선택가격");
+		ta_seat_info.setText("좌석정보");
+		ta_movie_info.setText("영화정보");
 	
 		
 		//위치
-		select_pay_ta.setBounds(784, 815, 196, 126);
-		seat_info_ta.setBounds(536, 815, 170, 126);
-		movie_info_ta.setBounds(349, 815, 96, 126);
-		movie_image.setBounds(259, 869, 62, 18);
+		ta_select_pay.setBounds(784, 815, 196, 126);
+		ta_seat_info.setBounds(536, 815, 170, 126);
+		ta_movie_info.setBounds(349, 815, 96, 126);
+		la_movie_image.setBounds(259, 869, 62, 18);
 		
 		//contentpane
-		getContentPane().add(select_pay_ta);
-		getContentPane().add(seat_info_ta);
-		getContentPane().add(movie_image);
-		getContentPane().add(movie_info_ta);
+		getContentPane().add(ta_select_pay);
+		getContentPane().add(ta_seat_info);
+		getContentPane().add(la_movie_image);
+		getContentPane().add(ta_movie_info);
 		
 
 		
