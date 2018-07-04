12e2c84b675434a681bb3ec41187f65fd4f67fad
park <pjhyng0125@naver.com>
2018-07-04 11:17:07 +0900

    진형 update scheduleView
    delete check function

--- cinema/src/com/playdata/view/ScheduleView.java
+++ cinema/src/com/playdata/view/ScheduleView.java
@@ -2,6 +2,7 @@ package com.playdata.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,6 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 /*
  * 영화스케쥴뷰
  */
@@ -75,44 +77,4 @@ public class ScheduleView extends JFrame {
 		setSize(1200, 800);
 		setVisible(false);
 	}//생성자
-	
-	/*
-	 * 작성자: 박진형
-	 * 수정일자: 07/03 20:32
-	 * 이벤트리스너 기능: ScheduleDateView num check
-	 */
-	public void canChecksDate() {	//한개면 그것 빼고 다 Enable(false) & 0개면 다 Enable(true) 
-//		int checked = -1;
-//		int count = 0;
-//		for(int i=0; i<v_sd.length; i++)
-//			if(v_sd[i].isSelected()) {
-//				checked = i;
-//				count++;
-//			}
-//		
-//		for(int i=0; i<v_sd.length; i++) {
-//			if(count == 1)
-//				if(checked != i)
-//					v_sd[i].setEnabled(false);
-//			else if(count == 0)
-//					v_sd[i].setEnabled(true);
-//		}
-	}
-	
-	/*
-	 * 작성자: 박진형
-	 * 수정일자: 07/03 20:32
-	 * 이벤트리스너 기능: ScheduleTimeView num check
-	 */
-	public boolean canChecksTime() {
-		int n = 0;
-		flag = true;
-		for(int i=0; i<v_st.length; i++)
-			if(v_st[i].tbt_time.isSelected())
-				n++;
-		if(n>1)
-			flag = false;
-		return flag;
-	}
-
 }
