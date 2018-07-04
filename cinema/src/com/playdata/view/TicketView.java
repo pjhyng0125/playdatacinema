6a9b20b8b9e214d02bd7c0aae9533d9c157e5adf
wooyoung92 <40686749+wooyoung92@users.noreply.github.com>
2018-07-04 20:20:26 +0900

    우영
    reserveview 에 사용할 이미지 4장
    rewview에 사용할 이미지 4장
    screenview에 사용할 이미지 3장 추가
    ------------------------------------------
    cashview 텍스트필드,라벨,패널 추가 및
    배치 변경
    ticketview 색 변경 및 사이즈 변경
    myhistoryview 색 변경 고객 이미지 추가

--- cinema/src/com/playdata/view/TicketView.java
+++ cinema/src/com/playdata/view/TicketView.java
@@ -8,6 +8,7 @@ import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 /*
  * ¸ð¹ÙÀÏÆ¼ÄÏºä
  */
@@ -22,54 +23,57 @@ public class TicketView extends JFrame {
 	public TicketView() {
 		setTitle("TicketView");
 		
-		la_ticket = new JLabel("¸ð¹ÙÀÏ Æ¼ÄÏ");		
-		la_ticket.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_ticket.setBounds(70, 15, 300, 80);
+		la_ticket = new JLabel("¸ð¹ÙÀÏ Æ¼ÄÏ");
+		la_ticket.setForeground(Color.WHITE);
+		la_ticket.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 22));
+		la_ticket.setBounds(14, 12, 172, 80);
 		la_reserver = new JLabel("¿µÈ­ ¿¹¸ÅÀÚ : ");
 		la_reserver.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_reserver.setBounds(20,20,200,70);
+		la_reserver.setBounds(14,39,200,70);
 		la_movie = new JLabel("¿¹¸Å ¿µÈ­     : ");
 		la_movie.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_movie.setBounds(20, 80, 200, 70);
+		la_movie.setBounds(14, 111, 200, 70);
 		la_date = new JLabel("°ü¶÷ ÀÏÀÚ     : ");
 		la_date.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_date.setBounds(20, 140, 200, 70);
+		la_date.setBounds(14, 182, 200, 70);
 		la_time = new JLabel("°ü¶÷ ½Ã°¢     :");
 		la_time.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_time.setBounds(20, 200, 200, 70);
+		la_time.setBounds(14, 253, 200, 70);
 		la_person = new JLabel("¿¹¸Å ÀÎ¿ø     : ");
 		la_person.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_person.setBounds(20, 260, 200, 70);
+		la_person.setBounds(14, 325, 200, 70);
 		la_seat = new JLabel("¿¹¸Å ÁÂ¼®     : ");
 		la_seat.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_seat.setBounds(20, 320, 200, 70);
+		la_seat.setBounds(14, 397, 200, 70);
 		
 		la_reserver2 = new JLabel("È«±æµ¿´Ô");
-		la_reserver2.setBounds(230, 20, 300, 70);
+		la_reserver2.setBounds(224, 39, 244, 70);
 		la_reserver2.setFont(new Font("¸¼Àº°íµñ", 0, 30));
 		la_movie2 = new JLabel("È«±æµ¿Àü");
 		la_movie2.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_movie2.setBounds(230, 80, 300, 70);
+		la_movie2.setBounds(224, 111, 244, 70);
 		la_date2 = new JLabel("2018³â 6¿ù 29ÀÏ ");
 		la_date2.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_date2.setBounds(230, 140, 300, 70);
+		la_date2.setBounds(224, 182, 244, 70);
 		la_time2 = new JLabel("19½Ã 40ºÐ");
 		la_time2.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_time2.setBounds(230, 200, 300, 70);
+		la_time2.setBounds(224, 253, 244, 70);
 		la_person2 = new JLabel("¼ºÀÎ 1¸í");
 		la_person2.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_person2.setBounds(230, 260, 300, 70);
+		la_person2.setBounds(224, 325, 244, 70);
 		la_seat2 = new JLabel("1»ó¿µ°ü B¿­ 4¼®");
 		la_seat2.setFont(new Font("¸¼Àº°íµñ", 0, 30));
-		la_seat2.setBounds(230, 320, 300, 70);
+		la_seat2.setBounds(224, 397, 244, 70);
 		
 		p_north = new JPanel();
+		p_north.setForeground(Color.WHITE);
 		p_north.setLayout(null);
 		p_north.setPreferredSize(new Dimension(500, 100));
-		p_north.setBackground(Color.blue);
+		p_north.setBackground(Color.BLACK);
 		p_north.add(la_ticket);
 		
 		p_center = new JPanel();
+		p_center.setBackground(Color.WHITE);
 		p_center.setLayout(null);
 		p_center.setPreferredSize(new Dimension(500, 600));
 		p_center.add(la_reserver);
@@ -86,13 +90,16 @@ public class TicketView extends JFrame {
 		p_center.add(la_person2);
 		p_center.add(la_seat2);
 		
-		setLayout(new BorderLayout());
-		add("North",p_north);
-		add("Center",p_center);
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add("North",p_north);
+		getContentPane().add("Center",p_center);
 		
 		
 		setSize(500, 700);
 		setVisible(true);
 	}//»ý¼ºÀÚ
+	public static void main(String[] args) {
+		new TicketView();
+	}
 	
 }
