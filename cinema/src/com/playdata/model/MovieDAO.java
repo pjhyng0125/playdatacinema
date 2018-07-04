ca0e302b7c5cdb0794ed0ffee072559181a8ab54
Phyungjin <40713258+Phyungjin@users.noreply.github.com>
2018-07-04 21:30:58 +0900

    movieDAO(초기단계)(형진)

--- cinema/src/com/playdata/model/MovieDAO.java
+++ cinema/src/com/playdata/model/MovieDAO.java
@@ -1,11 +1,62 @@
 package com.playdata.model;
+
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /*
  * 무비DAO: 영화의 정보 - DB
  */
 public class MovieDAO {
 	
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	Properties pro;
+	
 	public MovieDAO() {
-		System.out.println("git 연습 발견시 삭제하고 프로그래밍 하시면 됩니다.");
+		try {
+			pro = new Properties();
+			pro.load(new FileReader("conn/conn.properties"));
+			Class.forName(pro.getProperty("driver"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void movieSelectAll() {//배열로 파라미터 받기
+		connect();
 		
+		
+		
+	}
+	public void insert() {
+		
+	}
+	public void update() {
+		
+	}
+	public void delete() {
+		
+	}
+	
+	private void connect() {
+		try {
+			conn = DriverManager.getConnection(pro.getProperty("url"), pro);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void disconnect() {
+		try {
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
